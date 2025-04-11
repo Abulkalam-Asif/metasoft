da09c4b76c67a5e137c9941d8bb5e1c245a2613e
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2025-04-11 14:38:47 +0500

    Fix import path for Login component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
-import Login from "./screens/login";
+import Login from "./screens/Login";
 const App = () => {
   return (
     <>
